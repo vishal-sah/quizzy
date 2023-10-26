2549f476435b30df22509f7162ddb3f8134853a8
Vishal Kumar Sah <65996746+vishal-sah@users.noreply.github.com>
2023-10-26 11:17:56 +0545

    Updated the homepage

--- lib/gradient_color_container.dart
+++ lib/gradient_color_container.dart
@@ -11,7 +11,7 @@ class GradientColorContainer extends StatelessWidget {
   final Color color2;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -4,15 +4,37 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
           'assets/images/quiz-logo.png',
-          width: 20,
+          width: 200,
         ),
-        // Text('Learn Flutter the fun way',),
+        const SizedBox(
+          height: 40,
+        ),
+        const Text(
+          'Learn Flutter the fun way!',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        OutlinedButton(
+          onPressed: () {},
+          child: const Text(
+            'Start Quiz',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+            ),
+          ),
+        )
       ],
     );
   }
